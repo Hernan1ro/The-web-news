33b51151c9e4af5b780033aadb587773969f815b
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-27 16:19:12 -0500

    getting the images and posts

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const menu = document.querySelector(".information-menu");
 const newsContainer = document.querySelector(".news-container");
 //-----------LISTENERS-----------//
 window.onload = () => {
-  getData();
+  getPosts();
   console.log("Obteniendo datos...");
 };
 menuIcon.addEventListener("click", () => {
@@ -23,32 +23,63 @@ function handleMenu(boolean) {
     menu.classList.remove("menu-active");
   }
 }
-function getData() {
-  const URL = "https://jsonplaceholder.typicode.com/posts";
-  fetch(URL)
-    .then((response) => response.json())
-    .then((response) => printNewsOnDOM(response));
+function getPosts() {
+  let posts;
+  const POSTS = "https://jsonplaceholder.typicode.com/posts";
+  const PHOTOS = "https://jsonplaceholder.typicode.com/photos";
+  // Call the API
+  fetch(POSTS)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
+    })
+    .then(function (data) {
+      // Store the post data to a variable
+      posts = data;
+      // Fetch another API
+      return fetch(PHOTOS);
+    })
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
+    })
+    .then((images) => printNewsOnDOM(posts, images))
+    .catch(function (error) {
+      console.warn(error);
+    });
 }
-function printNewsOnDOM(data) {
-  data.map((item) => {
-    const { userId, title, id, body } = item;
-    const article = document.createElement("article");
-    article.classList.add("new");
-    const img = document.createElement("img");
-    const h2 = document.createElement("h2");
-    const span = document.createElement("span");
-    const p = document.createElement("p");
-    const button = document.createElement("button");
-    h2.innerText = title;
-    span.innerText = "Posted on January 7, 2008 by admin";
-    p.innerText = body;
-    button.innerText = "Continue reading";
 
-    article.appendChild(img);
-    article.appendChild(h2);
-    article.appendChild(span);
-    article.appendChild(p);
-    article.appendChild(button);
-    newsContainer.appendChild(article);
+function printNewsOnDOM(post, images) {
+  console.log(post, images);
+  return;
+  data.map((item, index) => {
+    if (index <= 100) {
+      const { userId, title, id, body } = item;
+      const article = document.createElement("article");
+      article.classList.add("new");
+      article.dataset.id = id;
+      article.dataset.userId = userId;
+      const img = document.createElement("img");
+      const h2 = document.createElement("h2");
+      const span = document.createElement("span");
+      const p = document.createElement("p");
+      const button = document.createElement("button");
+      h2.innerText = title;
+      span.innerText = "Posted on January 7, 2008 by admin";
+      p.innerText = body;
+      button.innerText = "Continue reading";
+      article.appendChild(img);
+      article.appendChild(h2);
+      article.appendChild(span);
+      article.appendChild(p);
+      article.appendChild(button);
+      newsContainer.appendChild(article);
+    }
   });
 }
